2c085b7acedfbc4bfa5df8a12137f86e8b29fe48
Kalin1603 <kalintodorov03@gmail.com>
2021-02-14 11:52:01 +0200

    Created method GetOrCreateTag in Services

--- CarsVelingrad/CarsVelingrad.Services/VehicleServices.cs
+++ CarsVelingrad/CarsVelingrad.Services/VehicleServices.cs
@@ -1,11 +1,30 @@
 ﻿namespace CarsVelingrad.Services
 {
+    using CarsVelingrad.Data;
+    using CarsVelingrad.Data.Models;
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using System.Text;
 
     public class VehicleServices
     {
+        private readonly ApplicationDbContext db;
 
+        public VehicleServices(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        private Tag GetOrCreateTag(string tagName)
+        {
+            Tag tag = this.db.Tags.FirstOrDefault(x => x.Name == tagName);
+
+            if (tag == null)
+            {
+                tag = new Tag() { Name = tagName };
+            }
+            return tag;
+        }
     }
 }
