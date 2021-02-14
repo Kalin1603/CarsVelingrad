a5b17a53a1aa7a04ad76dfa494db667d77dac5e1
Marian Halachev <mariqn03032@gmail.com>
2021-02-14 11:54:06 +0200

    Added VehicleServices

--- CarsVelingrad/CarsVelingrad.Services/VehicleServices.cs
+++ CarsVelingrad/CarsVelingrad.Services/VehicleServices.cs
@@ -1,11 +1,23 @@
 ﻿namespace CarsVelingrad.Services
 {
+    using CarsVelingrad.Data;
+    using CarsVelingrad.Data.Models;
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using System.Text;
 
     public class VehicleServices
     {
+        private readonly ApplicationDbContext db;
 
+        public VehicleServices(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        
+
+        }
     }
 }
