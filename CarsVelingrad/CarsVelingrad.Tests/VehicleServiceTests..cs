4c6ceeb76c5eec0779524b86710c7ae776b12d23
Kalin1603 <kalintodorov03@gmail.com>
2021-03-18 14:05:32 +0200

    Still not fixed

--- CarsVelingrad/CarsVelingrad.Tests/VehicleServiceTests..cs
+++ CarsVelingrad/CarsVelingrad.Tests/VehicleServiceTests..cs
@@ -4,25 +4,26 @@ using CarsVelingrad.Services;
 using Microsoft.EntityFrameworkCore;
 using Moq;
 using NUnit.Framework;
-using System.Collections.Generic;
-using System.Linq;
-
 namespace CarsVelingrad.Tests
 {
+    using System.Collections.Generic;
+    using System.Linq;
+
     public class VehicleServiceTests
     {
         IQueryable<Vehicle> data = new List<Vehicle>
             {
-                new Vehicle {Id=1, Price = 15000, },
-                new Vehicle {Id=2, Price = 25000,  },
-                new Vehicle {Id=3, Price = 55000,  },
-                new Vehicle {Id=4, Price = 125000,  },
-                new Vehicle {Id=5, Price = 65000,  },
+                new Vehicle {Id=1, Price = 15000, Model = new Model() { Name = "BMW"}, City = new City() {Name="Blagoevgrad"}, Run = 10000, Engine = new Engine(){ Volume = 2000}},
+                new Vehicle {Id=2, Price = 25000, Model = new Model() {Name = "Audi"}, City = new City() {Name="Pazarjik"}, Run = 11000, Engine = new Engine(){ Volume = 2500} },
+                new Vehicle {Id=3, Price = 55000, Model = new Model() {Name = "GF"}, City = new City() {Name="Plovdiv"}, Run = 10400, Engine = new Engine(){ Volume = 2030} },
+                new Vehicle {Id=4, Price = 125000,Model = new Model() {Name = "VW"}, City = new City() {Name="Varna"}, Run = 10000, Engine = new Engine(){ Volume = 3000}  },
+                new Vehicle {Id=5, Price = 65000, Model = new Model() {Name = "Opel"}, City = new City() {Name="Burgas"}, Run = 10300, Engine = new Engine(){ Volume = 4000} },
             }.AsQueryable();
 
         [SetUp]
         public void Setup()
         {
+
         }
 
         [Test]
